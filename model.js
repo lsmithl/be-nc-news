dfdecda9d3687b50bb20bef48e3efb2b0bb300f2
lsmithl <smithl42@hotmail.com>
2025-01-31 12:30:35 +0000

    add feat GET /api/articles/:article_id (comment_count)

--- model.js
+++ model.js
@@ -68,7 +68,24 @@ exports.selectArticles = ({
 
 exports.selectArticlesById = ({ article_id }) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+    .query(
+      `SELECT
+      articles.author,
+      articles.title,
+      articles.article_id,
+      articles.body,
+      articles.topic,
+      articles.created_at,
+      articles.votes,
+      articles.article_img_url,
+      COUNT(comments.article_id) :: INTEGER AS comment_count
+      FROM articles
+      LEFT JOIN comments
+      ON articles.article_id = comments.article_id
+      WHERE articles.article_id = $1
+      GROUP BY articles.article_id;`,
+      [article_id]
+    )
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Not Found" });
